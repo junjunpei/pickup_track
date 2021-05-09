476c57ea30e3ed34da0c3f8629052d1e1dff6eeb
大村純平 <junpei.aug07@gmail.com>
2021-05-09 16:19:27 +0900

    Add: webpackerでbootstrap、jquery、popper.jsの導入及び適用 #5

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,6 @@
+import 'bootstrap';
+import '../stylesheets/application';
+
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
@@ -7,7 +10,6 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
