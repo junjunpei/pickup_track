b21fb506db4ef66268decd0348a04d5781fa9e68
大村純平 <junpei.aug07@gmail.com>
2021-05-14 16:32:41 +0900

    Add: usersコントローラーとuser_sessionsコントローラーの作成、ルーティングの設定 #6

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root 'static_pages#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users
+  get    'login',  to: 'user_session#new',     as: :login
+  post   'login',  to: 'user_session#create'
+  delete 'logout', to: 'user_session#destroy', as: :logout
 end
