8b6403b2986a45e7c317a6912942bf11941b2843
大村純平 <junpei.aug07@gmail.com>
2021-05-14 17:00:22 +0900

    Add: application_controllerにnot_authenticatedメソッドを追加 #6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :require_login
+
+  private
+  def not_authenticated
+    redirect_to login_path, alert: 'ログインしてください'
+  end
 end
