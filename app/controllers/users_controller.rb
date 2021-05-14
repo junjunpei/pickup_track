09d860e0a2024cc4ac90bf8a7f6b9d0b342afae4
大村純平 <junpei.aug07@gmail.com>
2021-05-14 17:30:46 +0900

    Add: users_controllerのnewアクションとcreateアクションの追加 #6

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,21 @@
 class UsersController < ApplicationController
   skip_before_action :require_login, only: %i[new create]
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      auto_login(@user)
+      redirect_to '#', success: 'ユーザーを登録しました'
+    else
+      flash.now[:alert] = '登録に失敗しました'
+      render :new
+    end
+  end
+
   private
 
   def user_params
