46f99a1d675ed2e4cdc6cc49069dc046ed090075
大村純平 <junpei.aug07@gmail.com>
2021-05-16 16:55:30 +0900

    Fix: ストロングパラメーターにnameを追加 #6

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      auto_login(@user)
+      # auto_login(@user)
       redirect_to '#', success: 'ユーザーを登録しました'
     else
       flash.now[:alert] = '登録に失敗しました'
@@ -19,6 +19,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
