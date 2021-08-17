e72c44bd7ad711c5409c3fe3ccb937f43a534dc5
大村純平 <junpei.aug07@gmail.com>
2021-08-17 16:35:45 +0900

    Fix: フラッシュメッセージがヘッダーに重ならないよう調整 #24

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
       auto_login(@user)
       redirect_to root_path, success: 'ユーザーを登録しました'
     else
-      flash.now[:alert] = '登録に失敗しました'
+      flash.now[:danger] = '登録に失敗しました'
       render :new
     end
   end
