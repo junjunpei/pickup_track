1144ab89ab273eabd4f2d1efe8338f739bb54dbb
大村純平 <junpei.aug07@gmail.com>
2021-08-20 13:06:55 +0900

    Add: application.rbにi18nの設定を追加 #9

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,11 @@ module SongShuffle
       g.skip_routes true
     end
 
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
