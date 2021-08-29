40b7ab54c43f9b6e8e627b903728f2df27df2b18
大村純平 <junpei.aug07@gmail.com>
2021-08-29 17:25:52 +0900

    Fix: ログイン中にログイン画面に飛べないようにする #29

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,7 +1,9 @@
 class UserSessionsController < ApplicationController
   skip_before_action :require_login, only: %i[new create]
 
-  def new; end
+  def new
+    redirect_to root_path, info: 'ログインしています' if logged_in?
+  end
 
   def create
     @user = login(params[:email], params[:password])
