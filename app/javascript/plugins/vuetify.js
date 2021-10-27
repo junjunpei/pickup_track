02634d2b92d1333e6a6706b7c3f345c68fffa1bf
大村純平 <junpei.aug07@gmail.com>
2021-10-27 16:57:51 +0900

    Add: マテリアルアイコンのインストール #37

--- app/javascript/plugins/vuetify.js
+++ app/javascript/plugins/vuetify.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import '@mdi/font/css/materialdesignicons.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.use(Vuetify)
 
