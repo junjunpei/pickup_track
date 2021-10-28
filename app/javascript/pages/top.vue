112143a2be7eb459ea4d3eacabe4fcd70e607d02
大村純平 <junpei.aug07@gmail.com>
2021-10-28 17:52:18 +0900

    Add: ヘッダーコンポーネントを作成し、トップ画面のヘッダー部分を移動 #39

--- app/javascript/pages/top.vue
+++ app/javascript/pages/top.vue
@@ -1,26 +1,5 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="black"
-      height="80"
-    >
-      <v-avatar
-        class="mr-3"
-        color="grey lighten-5"
-        size="30"
-      >
-        <v-img
-          contain
-          max-height="70%"
-        ></v-img>
-      </v-avatar>
-
-      <v-toolbar-title class="font-weight-black headline">
-        Song Shuffle
-      </v-toolbar-title>
-    </v-app-bar>
-
     <v-content>
       <section id="hero">
         <v-row no-gutters>
