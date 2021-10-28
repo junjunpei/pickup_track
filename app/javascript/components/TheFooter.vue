a9ad0f30d08de7f90df3d496237f3d74cc8974b5
大村純平 <junpei.aug07@gmail.com>
2021-10-28 22:34:41 +0900

    Add: フッターコンポーネントを作成し、トップ画面のフッター部分を移動 #39

---
+++ app/javascript/components/TheFooter.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-footer
+    class="justify-center"
+    color="#292929"
+    height="100"
+  >
+    <div class="title font-weight-light grey--text text--lighten-1 text-center">
+      &copy; {{ (new Date()).getFullYear() }} — Vuetify, LLC — Made with 💜 by John Leider
+    </div>
+  </v-footer>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+</style>
--- app/javascript/pages/top.vue
+++ app/javascript/pages/top.vue
@@ -280,16 +280,6 @@
         <div class="py-12"></div>
       </v-sheet>
     </v-content>
-
-    <v-footer
-      class="justify-center"
-      color="#292929"
-      height="100"
-    >
-      <div class="title font-weight-light grey--text text--lighten-1 text-center">
-        &copy; {{ (new Date()).getFullYear() }} — Vuetify, LLC — Made with 💜 by John Leider
-      </div>
-    </v-footer>
   </div>
 </template>
 
