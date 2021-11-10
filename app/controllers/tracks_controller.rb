7e80716ff38530c6da6b3b01308c7e25becb702f
大村純平 <junpei.aug07@gmail.com>
2021-11-10 15:20:13 +0900

    Fix: rubocopの指摘箇所を修正 #33

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,8 +1,8 @@
 class TracksController < ApplicationController
   def index
-    if params[:search].present?
-      result = RSpotify::Track.search(params[:search])
-      @tracks = Kaminari.paginate_array(result).page(params[:page]).per(10)
-    end
+    return unless params[:search].present?
+
+    result = RSpotify::Track.search(params[:search])
+    @tracks = Kaminari.paginate_array(result).page(params[:page]).per(10)
   end
 end
--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -4,7 +4,7 @@
 # Available submodules are: :user_activation, :http_basic_auth, :remember_me,
 # :reset_password, :session_timeout, :brute_force_protection, :activity_logging,
 # :magic_login, :external
-Rails.application.config.sorcery.submodules = [:remember_me, :reset_password]
+Rails.application.config.sorcery.submodules = %i[remember_me reset_password]
 
 # Here you can configure each submodule's features.
 Rails.application.config.sorcery.configure do |config|
@@ -226,7 +226,6 @@ Rails.application.config.sorcery.configure do |config|
   # config.line.bot_prompt = "normal"
   # config.line.user_info_mapping = {name: 'displayName'}
 
-  
   # For information about Discord API
   # https://discordapp.com/developers/docs/topics/oauth2
   # config.discord.key = "xxxxxx"
@@ -553,5 +552,5 @@ Rails.application.config.sorcery.configure do |config|
 
   # This line must come after the 'user config' block.
   # Define which model authenticates with sorcery.
-  config.user_class = "User"
+  config.user_class = 'User'
 end
