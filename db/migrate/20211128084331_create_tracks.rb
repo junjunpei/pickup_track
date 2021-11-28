324009a1ca6f8d836fbb500662fac3a9b7f0609f
大村純平 <junpei.aug07@gmail.com>
2021-11-28 18:48:26 +0900

    Add: tracksモデルとマイグレーションファイルの追加 #11

---
+++ db/migrate/20211128084331_create_tracks.rb
@@ -0,0 +1,11 @@
+class CreateTracks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tracks do |t|
+      t.integer :track_id, null: false
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+      t.index [:user_id, :track_id], unique: true
+    end
+  end
+end
