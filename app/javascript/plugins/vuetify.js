21a79d7410f2d434a8e4cb208b5c7725cc69daf0
大村純平 <junpei.aug07@gmail.com>
2022-02-05 11:52:30 +0900

    Remove: Material iconsの削除

--- app/javascript/plugins/vuetify.js
+++ app/javascript/plugins/vuetify.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import '@mdi/font/css/materialdesignicons.css'
-import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.use(Vuetify)
 
